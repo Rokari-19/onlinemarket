8422dc39f347c2b846970510b53ebd3c6c81d243
rokarioss <oshomahiyelobu1@gmail.com>
2023-12-21 15:00:27 +0100

    made responsive, updates coming soon

--- onlinemarket/settings.py
+++ onlinemarket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$(*=d3m$bdvhi)x@ecmp)$#r719b5b=7f8ukg1sz-^v1g*sl$f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 LOGIN_URL = '/login/'
