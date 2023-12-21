7d7907aaec57095122e4f43ee59b1730967bbea3
rokarioss <oshomahiyelobu1@gmail.com>
2023-12-21 21:53:56 +0100

    made a change to settings.py

--- onlinemarket/settings.py
+++ onlinemarket/settings.py
@@ -96,13 +96,7 @@ DATABASES = {
 }
 
 # Update database configuration from $DATABASE_URL environment variable (if defined)
-import dj_database_url
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        conn_max_age=500,
-        conn_health_checks=True,
-    )
 
 
 
